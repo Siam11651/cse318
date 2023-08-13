3b2f4369cd554829a027bd2c174d9d4b422b4d09
Siam11651 <siam11651@outlook.com>
2023-08-13 23:31:55 +0600

    kaaj kori

--- offline-3/src/main.cpp
+++ offline-3/src/main.cpp
@@ -6,14 +6,23 @@
 
 int main()
 {
-    offline_3::graph graph(3);
+    size_t vertices_count, edges_count;
 
-    graph.add_edge(1, 2, 1);
-    graph.add_edge(2, 1, 1);
-    graph.add_edge(2, 3, 2);
-    graph.add_edge(3, 2, 2);
-    graph.add_edge(3, 1, 3);
-    graph.add_edge(1, 3, 3);
+    std::cin >> vertices_count >> edges_count;
+
+    offline_3::graph graph(vertices_count);
+
+    for(size_t i = 0; i < edges_count; ++i)
+    {
+        uint64_t from;
+        uint64_t to;
+        int64_t weight;
+
+        std::cin >> from >> to >> weight;
+
+        graph.add_edge(from, to, weight);
+        graph.add_edge(to, from, weight);
+    }
 
     offline_3::maxcut maxcut = offline_3::maxcut_solver::get_maxcut(graph);
 
