ab6583d6945e4262bec77c98b3e19d6358fe7341
Siam11651 <siam11651@outlook.com>
2023-08-21 03:45:20 +0600

    kaaj kori

--- offline-3-table-generator/main.py
+++ offline-3-table-generator/main.py
@@ -9,12 +9,12 @@ upper_bounds_json_object = json.loads(upper_bounds_json_string)
 def gen_table(algorithm: str):
     geometry_options = "a4paper, tmargin=1cm, lmargin=1cm"
     document = Document(geometry_options=geometry_options)
-    table = Tabular("| c | c | c | c | c |")
+    table = Tabular("| c | c | c | c | c | c |")
 
     table.add_hline()
-    table.add_row((MultiRow(size=2, data="g"), MultiColumn(size=4, align="| c |", data=formatted_algorithm_name[algorithm])))
-    table.add_hline(2, 5)
-    table.add_row(("", "Before Local Search", "After Local Search", "Local Search Iterations", "Upper Bound"))
+    table.add_row((MultiRow(size=2, data="g"), MultiColumn(size=5, align="| c |", data=formatted_algorithm_name[algorithm])))
+    table.add_hline(2, 6)
+    table.add_row(("", "Before Search", "After Search", "Local Search Iterations", "GRASP Iterations", "Upper Bound"))
     table.add_hline()
 
     for i in range(1, 55):
@@ -22,7 +22,7 @@ def gen_table(algorithm: str):
         line = file.readline()
         (after_search, before_search, search_iteration) = line.split(" ")   # one line in one output file corresponding a test input data
 
-        table.add_row((i, before_search, after_search, search_iteration, upper_bounds_json_object[i - 1]))
+        table.add_row((i, before_search, after_search, search_iteration, 50, upper_bounds_json_object[i - 1]))
         table.add_hline()
     
     document.append(table)
